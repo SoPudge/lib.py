eea93abbff2099f6b95671576d8dd0d46c2b82a1
SoPudge <abcd666@gmail.com>
2019-03-01 16:30:06 +0800

    [MOD]新增日志可以通过命名关键字参数，定义存储位置和日志文件名称

--- logger.py
+++ logger.py
@@ -1,20 +1,36 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
+
+import os
 import sys
 import logging
 
 class Logger(object):
-    def __init__(self,logger):
-        """ 
+    def __init__(self,logger,**kw):
+        '''
         creat a logger class,port from default loggin module
-        """
+        Description:
+            对于不传入**kw参数的调用，为旧式调用，用于一些旧式程序
+            对于传输**kw的掉用，则支持如下功能
+            1、定义日志的存储位置
+            2、日志名称命名方式：以程序命名，以电脑命名
+        '''
+        #**kw参数定义
+        if len(kw) == 0:
+            #获取当前文件所在的目录
+            file_path = sys.path[0]
+            file_name = 'log.log'
+        else:
+            file_path = kw['file_path']
+            file_name = kw['file_name']
+        #**kw参数定义结束
+
         #creat a logger
         self.logger = logging.getLogger(logger)
         self.logger.setLevel(logging.DEBUG)
 
         # creat a handler to output log to file
-        log_name = 'log.log'
-        fh = logging.FileHandler(sys.path[0] + '/' + log_name)
+        fh = logging.FileHandler(file_path + '/' + file_name)
         fh.setLevel(logging.INFO)
 
         # creat a handler to output log to stream 
@@ -46,5 +62,6 @@ class Logger(object):
         self.logger.removeHandler(self.fh)
         
 if __name__ == '__main__':
+    #kw = {'file_path':'N:\\005_Software','file_name':'abc.log'}
     logger = Logger('mylog')
     logger.info('this is in log')
